5f66ed41291b4abae8057804634b3723bdfeb731
sxy <731727718@qq.com>
2017-05-28 00:28:18 +0800

    TabLayout的布局搭建

--- app/src/main/java/com/example/xy/smartbutler/application/BaseApplication.java
+++ app/src/main/java/com/example/xy/smartbutler/application/BaseApplication.java
@@ -12,7 +12,7 @@ import android.app.Application;
  */
 public class BaseApplication extends Application {
 
-    //创建1
+    //创建11
    @Override
     public void onCreate() {
        super.onCreate();
