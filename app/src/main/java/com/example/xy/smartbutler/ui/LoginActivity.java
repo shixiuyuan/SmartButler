50fa6116357586e3db5ad7c7f2f12259f699884c
sxy <731727718@qq.com>
2017-06-01 09:16:45 +0800

    用户登录功能

--- app/src/main/java/com/example/xy/smartbutler/ui/LoginActivity.java
+++ app/src/main/java/com/example/xy/smartbutler/ui/LoginActivity.java
@@ -3,10 +3,20 @@ package com.example.xy.smartbutler.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.xy.smartbutler.MainActivity;
 import com.example.xy.smartbutler.R;
+import com.example.xy.smartbutler.entity.MyUser;
+import com.example.xy.smartbutler.utils.ShareUtils;
+
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SaveListener;
 
 /*
  *   项目名: SmartButler
@@ -20,6 +30,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     //注册按钮
     private Button btn_registered;
+    private EditText et_name;
+    private EditText et_password;
+    private Button btnLogin;
+    private CheckBox keep_password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +46,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private void initView(){
         btn_registered = (Button)findViewById(R.id.btn_registered );
         btn_registered.setOnClickListener(this);
+        et_name = (EditText) findViewById(R.id.et_name);
+        et_password = (EditText) findViewById(R.id.et_password);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+        keep_password = (CheckBox) findViewById(R.id.keep_password);
+
+        //没值默认不选中记住密码
+        boolean isKeep = ShareUtils.getBoolean(this, "keeppass", false);
+        keep_password.setChecked(isKeep);
+
+        if(isKeep){
+            String name = ShareUtils.getString(this, "name", "");
+            String password = ShareUtils.getString(this, "password", "");
+            et_name.setText(name);
+            et_password.setText(password);
+        }
     }
 
     @Override
@@ -39,6 +70,73 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             case R.id.btn_registered:
                 startActivity(new Intent(this,RegisteredActivity.class));
                 break;
+            case R.id.btnLogin:
+                //1.获取输入框的值
+                String name = et_name.getText().toString().trim();
+                String password = et_password.getText().toString().trim();
+                //2.判断是否为空
+                if (!TextUtils.isEmpty(name) & !TextUtils.isEmpty(password)) {
+                    //登录
+                    final MyUser user = new MyUser();
+                    user.setUsername(name);
+                    user.setPassword(password);
+                    user.login(new SaveListener<MyUser>() {
+
+                        @Override
+                        public void done(MyUser myUser, BmobException e) {
+                            //判断结果
+                            if (e == null) {
+                                //判断邮箱是否验证
+                                if (user.getEmailVerified()){
+                                    //跳转到主页
+                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                    finish();
+                                }else {
+                                    Toast.makeText(LoginActivity.this, "请前往邮箱验证", Toast.LENGTH_SHORT).show();
+                                }
+                            }else {
+                                Toast.makeText(LoginActivity.this, "登录失败：" + e.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }else {
+                    Toast.makeText(this, "输入框不能为空", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    //假设我现在输入用户名和密码，但是我不点击登录，而是直接退出了
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        //保存状态
+        ShareUtils.putBoolean(this, "keeppass", keep_password.isChecked());
+
+        //是否记住密码
+        if (keep_password.isChecked()) {
+            //记住用户名和密码
+            ShareUtils.putString(this, "name", et_name.getText().toString().trim());
+            ShareUtils.putString(this, "password", et_password.getText().toString().trim());
+        } else {
+            ShareUtils.deleShare(this, "name");
+            ShareUtils.deleShare(this, "password");
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
