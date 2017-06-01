33f0b52d368216749e88bd58b3305c80b3571860
sxy <731727718@qq.com>
2017-06-01 08:34:46 +0800

    Test

---
+++ app/src/main/java/com/example/xy/smartbutler/ui/aaaa.java
@@ -0,0 +1,14 @@
+package com.example.xy.smartbutler.ui;
+
+/*
+ *   项目名: SmartButler
+ *     包名:  com.example.xy.smartbutler.ui
+ *    文件名: aaaa
+ *  创建时间: 2017/6/1 8:33
+ *   描述 :   TODO
+ */
+public class aaaa {
+    public static void main(String[] args){
+        System.out.println("hello,world");
+    }
+}
